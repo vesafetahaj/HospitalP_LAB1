09d7d55fb14f03756ec39b9811f80170655fe2be
vesafetahaj <vf58160@ubt-uni.net>
2023-08-04 18:02:09 +0200

    Models modifications

---
+++ HOSPITAL2_LAB1/Models/Complaint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HOSPITAL2_LAB1.Models
+{
+    public partial class Complaint
+    {
+        public int ComplaintId { get; set; }
+        public DateTime? ComplaintDate { get; set; }
+        public string? ComplaintDetails { get; set; }
+        public int? Patient { get; set; }
+        public int? Administrator { get; set; }
+
+        public virtual Administrator? AdministratorNavigation { get; set; }
+        public virtual Patient? PatientNavigation { get; set; }
+    }
+}
---
+++ HOSPITAL2_LAB1/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HOSPITAL2_LAB1.Models
+{
+    public partial class Room
+    {
+        public int RoomId { get; set; }
+        public int? Patient { get; set; }
+
+        public virtual Patient? PatientNavigation { get; set; }
+    }
+}
