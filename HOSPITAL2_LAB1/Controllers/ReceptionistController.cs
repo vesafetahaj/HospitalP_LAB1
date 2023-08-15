e6ece82a3bd21e96e202a8cee22360dde50676cb
vesafetahaj <vf58160@ubt-uni.net>
2023-08-15 13:11:16 +0200

    Home page front-end included

---
+++ HOSPITAL2_LAB1/Controllers/ReceptionistController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HOSPITAL2_LAB1.Controllers
+{
+    public class ReceptionistController : Controller
+    {
+        // GET: ReceptionistController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ReceptionistController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReceptionistController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReceptionistController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReceptionistController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReceptionistController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReceptionistController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReceptionistController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
