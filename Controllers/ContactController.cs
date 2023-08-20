30a9aa8c21b1b74061791c1e0357387bbdbc7f5c
vesafetahaj <vf58160@ubt-uni.net>
2023-08-20 20:12:17 +0200

    Contact frontend included - needs improvment

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HOSPITAL2_LAB1.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
