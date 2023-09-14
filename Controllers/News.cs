bb8e6ebe04fa951160e71bb26a2a17e0eea7c6a8
Dea Fetahu <df56156@ubt-uni.net>
2023-09-14 17:48:04 +0200

    Added the news page

---
+++ Controllers/News.cs
@@ -0,0 +1,20 @@
+﻿using HOSPITAL2_LAB1.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HOSPITAL2_LAB1.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        
+
+        
+    }
+}
